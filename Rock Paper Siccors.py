edb97c19f0731ba6ab938ce0d3d0330a31e2f21d
Daniel <117318977+dankdaniel420@users.noreply.github.com>
2023-05-01 01:37:05 +0800

    Add files via upload
    As the CS50 guy says, its time to take the training wheels off and slowly start playing with the big boys.
    
    My first python coded game done in VS code. Decently proud of it as I had to take a break from coding for awhile due to work, and figured this project would refresh my understanding of object orientated programming.

---
+++ Rock Paper Siccors.py
@@ -0,0 +1,75 @@
+import random
+import time
+
+class Player:
+    def __init__ (self, name):
+        self.name = name
+        self.hand = ''
+        return
+        
+    def hands(self):
+        self.hand = ''
+        self.hand = input ('Choose rock, paper or siccors: ')
+        answers = ['rock', 'paper', 'siccors']
+        while self.hand not in answers:
+            self.hand = input ('Sorry, input invalid.\nChoose rock, paper or siccors: ')
+        return
+    
+class CPU:
+    def __init__(self):
+        self.hand = ''
+        return
+
+    def cpuhand(self):
+        answers = ['rock', 'paper', 'siccors']
+        random.shuffle(answers)
+        self.hand = answers[1]
+        return 
+
+def outcome(phand,chand):
+    print('ROCK')
+    time.sleep(1)
+    print('PAPER')
+    time.sleep(1)
+    print('SICCORS')
+    time.sleep(1)
+    print('SHOOT')
+
+    if phand == chand:
+        print(f'TIE!\nYour {phand} draws with CPUs {chand} ')
+        return
+    elif (phand == 'rock' and chand == 'siccors') or (phand == 'paper' and chand == 'rock') or (phand == 'siccors' and chand == 'paper'):
+        print(f'VICTORY!\nYour {phand} wins CPUs {chand} ')
+        return
+    else:
+        print(f'DEFEAT!\nYour {phand} loses to CPUs {chand} ')
+        return
+
+def gameon():
+    answer = ''
+    answer = input('Rematch? Y/N: ').capitalize()
+    while answer not in ['Y','N']:
+        answer = input('Sorry, input invalid.\nRematch? Y/N: ').capitalize()
+
+    if answer == 'Y':
+        return True
+    elif answer == 'N': 
+        print('Thanks for playing!! Goodbye')
+        return False
+
+def main():
+    playername = input ('Input username: ')
+    player = Player(playername)
+    computer = CPU()
+    game_on = True
+    while game_on:
+        player.hands()
+        computer.cpuhand()
+        outcome(player.hand, computer.hand)
+        game_on = gameon()
+    return
+
+main()
+
+
+
